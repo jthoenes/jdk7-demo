e378b911ee5ad68e386ff06fcfc92dfcd8ee074c
Johannes Thönes <johannes.thoenes@googlemail.com>
2011-03-09 18:09:20 +0100

    Changing Integer Literals for Blog-Post
      - Gigabyte as example for a large long
      - Show parsing of binary literals using Integer.parseInt
    
    Signed-off-by: Johannes Thönes <johannes.thoenes@googlemail.com>

--- src/main/java/de/bergischweb/kojug/jdk7/coin/IntegerLiterals.java
+++ src/main/java/de/bergischweb/kojug/jdk7/coin/IntegerLiterals.java
@@ -1,31 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package de.bergischweb.kojug.jdk7.coin;
 
-import org.junit.Assert;
+import static  org.junit.Assert.*;
 
 /**
  * @author Johannes Th&ouml;nes <johannes.thoenes@googlemail.com>
  */
 public class IntegerLiterals {
 
-  private long salary = 15_500_000_000L;
+  private long gigabyte = 1_073_741_824L;
   private int bitmask = 0b1010_1011;
     
     
     public static void main(String... args){
+        int binOld = Integer.parseInt("10011001", 2);        
+        
         int dec = 153;
         int hex = 0x99;
         int oct = 0231;
         int bin = 0b10011001;
         
-        Assert.assertEquals(dec, hex);
-        Assert.assertEquals(hex, oct);
-        Assert.assertEquals(oct, bin);
-        Assert.assertEquals(bin, dec);
+        assertEquals(dec, hex);
+        assertEquals(hex, oct);
+        assertEquals(oct, bin);
+        assertEquals(bin, dec);
+        
+        assertEquals(bin, binOld);
     }
 
 }
